570383c0e7219e4a88cafe4b80ced6e2e904a813
John Knowles <jknowles@bond.edu.au>
2020-11-13 16:59:00 +1000

    Added nav background switch on scroll

--- js/site.js
+++ js/site.js
@@ -1,6 +1,19 @@
-   M.AutoInit();
-  
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
-  });
+M.AutoInit();
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+});
+
+window.onscroll = function() {myFunction()};
+  var header = document.getElementById("main-nav");
+  var sticky = 500;
+  function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      header.classList.add("nav-bg-switch");
+    } else {
+      header.classList.remove("nav-bg-switch");
+    }
+}
+
+
